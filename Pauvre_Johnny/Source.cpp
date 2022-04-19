53d9d7cd1062f9a8d873efeb70434a297f43c6a7
Kancabricot <92451452+Kancabricot@users.noreply.github.com>
2022-04-19 18:06:12 +0200

    Fini enfin oui je les fini mais il faudrai que je repace dessus pour faire un system qui fait tout le txt sans nous demander quoi que se soit
    mais nique ça mere je le ferai si j'y pense et que j'ai le temp

--- Pauvre_Johnny/Source.cpp
+++ Pauvre_Johnny/Source.cpp
@@ -2,14 +2,53 @@
 #include <vector>
 #include <string>
 #include <map>
+#include <fstream>
 
 using namespace std;
 
-void nbOccurrences(char c, string str, map<char, int>& Mymap)
+void WriteTxt(map<char, int> Mymap) {
+
+    ofstream text;
+
+    text.open("txt/Occurrences.txt");
+    if (text) {
+        text << "Les Occurrences demander : " << endl ;
+        for (map<char, int>::iterator it = Mymap.begin(); it != Mymap.end(); it++) {
+            text << (*it).first << " = " << it->second << endl;
+        }
+    }
+    else {
+        cout << "erreur";
+    }
+}
+
+string ReadTxt() {
+
+    string text;
+    string test = "";
+
+    ifstream txt;
+
+    txt.open("txt/WildWildWest.txt");
+
+    if (txt) {
+        while (not txt.eof()) {
+            txt >> text;
+            test += text;
+        }
+    }
+    else {
+        cout << "erreur";
+    }
+
+    return test;
+}
+
+void nbOccurrences(char c, map<char, int>& Mymap)
 {
     int count = 0;
 
-    for (char i : str) {
+    for (char i : ReadTxt()) {
         if (i == c) {
             count++;
         }
@@ -23,17 +62,28 @@ int main() {
 
     map<char, int> Mymap;
 
-    char ch1 = 'e';
-    char ch2 = 'h';
+    int end = 1;
+    char chr;
 
-    string str1("zero e enfin la eeeeee ee e e e eil y en as 4");
-    string str2("pas de h enfin hla sh hmaish tg");
-    
-    nbOccurrences(ch1, str1, Mymap);
+    while (end == 1) {
+        cout << "quelle lettre voulez vous pour en connaitre le nombre de fois qu'elle est dans le fichier texte : ";
+
+        cin >> chr;
+
+        nbOccurrences(chr, Mymap);
+
+        cout << " voulez vous encore d'autre lettre/symbole ?" << endl << "Si oui : 1               Si non : 0" << endl;
+
+        cin >> end;
+    }
 
     for (map<char, int>::iterator it = Mymap.begin(); it != Mymap.end(); it++) {
         cout << (*it).first << " = " << it->second << endl;
     }
+
+    cout << "Tous vos demandes sont ecrit dans le fichier texte Occurences dans le dossier txt." << endl;
+
+    WriteTxt(Mymap);
 
     return 0;
 }
